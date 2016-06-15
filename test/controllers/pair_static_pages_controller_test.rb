ee6b4b2a63898a8f824596db64379dea8298fdd0
GettyGertie <gettyorawo@gmail.com>
2016-06-15 06:30:24 +0300

    Add title helper

--- test/controllers/pair_static_pages_controller_test.rb
+++ test/controllers/pair_static_pages_controller_test.rb
@@ -6,12 +6,16 @@ class PairStaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'h1', "Welcome to our Awesomeness"
     assert_select 'button', "Sign up now"
+    assert_select 'title', "Welcome to awesomeness"
   end
 
   test "should get help" do
     get pair_static_pages_help_url
     assert_response :success
     assert_select 'h1', "Our Help Page"
+    assert_select 'title', 'Welcome to awesomeness | Help'
   end
+
+
 
 end
